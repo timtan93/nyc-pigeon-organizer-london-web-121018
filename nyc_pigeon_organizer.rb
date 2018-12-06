ecddbf367314efce7e4b6d5ee19e6f5911bba1b3
timtan93 <timtan93@gmail.com>
2018-12-06 12:42:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
-def nyc_pigeon_organizer(data)
+require 'pry'
+def nyc_pigeon_organizer(hash)
   # write your code here!
+  pigeon_list ={}
+  hash.each do |data ,details|
+    details.each do |key ,value|
+      value.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][data] ||= []
+        pigeon_list[name][data] << key.to_s
+      end
+    end
+  end
+  pigeon_list
 end
